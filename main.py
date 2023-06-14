652dc1ea9fac2e3a0b40c60f9830ceb9d2f7eee1
SuzanaSSP <suzanasp1026@gmail.com>
2023-06-14 17:01:11 -0600

    Added repel function

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from agent import Agent
 import math
 
 # Simulation parameters
-L            = 50       # size of box
-M            = L//4		# smaller box
-dt           = 0.001     # time step
+min_pos      = 900      # minimum position of birds
+max_pos      = 1500       # max position of birds
+L            = 2000  	# size of box
 Nt           = 5000      # number of time steps
-N            = 2       # number of birds
+N            = 20      # number of birds
 plotRealTime = True
 
 
@@ -16,60 +16,43 @@ plotRealTime = True
 def main():
     
     # Create birds
-	agent = Agent(random.randint(0,L), random.randint(0,L),)
-	agents = [Agent(random.randint(0,L),random.randint(0,L),) for i in range(N)]
+	agent = Agent(random.randint(min_pos, max_pos), random.randint(min_pos, max_pos))
+	agents = [Agent(random.randint(min_pos, max_pos), random.randint(min_pos, max_pos)) for i in range(N)]
 	foreigners = [other_agent for other_agent in agents if other_agent is not agent]
    
     # Prep figure
-	fig = plt.figure(figsize=(L,L), dpi=96)
+	fig = plt.figure(figsize=(6,6), dpi=96)
 	ax = plt.gca()
  
 	for i in range(Nt):
-
-		# move
+   
 		for agent in agents:
-			agent.move()
-
+			agent.repel_function()
+   
 		# List of x and y values of birds to put in quiver
-		x = [agent.x for agent in agents if not math.isnan(agent.x)]
-		y = [agent.y for agent in agents if not math.isnan(agent.y)]
-		vx = [agent.vx for agent in agents if not math.isnan(agent.vx)]
-		vy = [agent.vy for agent in agents if not math.isnan(agent.vy)]
-
-		# Trying to expand the axis according to how the agents
-		"""
-		mean_of_x_pos = sum(x) / len(x)
-		mean_of_y_pos = sum(y) / len(y)
+		x = [agent.x for agent in agents]
+		y = [agent.y for agent in agents]
+		u = [agent.vx for agent in agents]
+		v = [agent.vy for agent in agents]
   
-		width_min = mean_of_x_pos//2
-		width_max = mean_of_x_pos*2
-		height_min = mean_of_y_pos//2
-		height_max = mean_of_y_pos*2
-		"""
-
-		# Make agent come back around if it leaves the axis
-		x = [i % L for i in x]
-		y = [i % L for i in y]
-
 		# plot in real time
 		if plotRealTime or (i == Nt-1):
 			# Clearing the axis and plotting again
 			plt.cla()
-			ax.quiver(x,y,vx,vy,color='r')
-			# Limits
-			ax.set_xlim(0, L)
-			ax.set_ylim(0, L)
+			ax.quiver(x,y,u,v, color='r')
+			ax.set_xlim(0,L)
+			ax.set_ylim(0,L)
 			# Pause per frame
-			plt.pause(0.0001)
-	
-		# update velocities
-		for agent in agents:
-			agent.update_velocity()
-			
+			plt.pause(0.001)
+   
 		# Update angular velocity 
 		for agent in agents:
 			agent.update_angular_velocity(foreigners)
-   			
+
+		# move
+		for agent in agents:
+			agent.move()
+		
 	# Save figure
 	plt.savefig('activematter.png',dpi=240)
 	plt.show()
