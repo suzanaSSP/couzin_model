ebd64c9c43510939b6b130e3d00b43bec51084c7
hgspig <42699788+hgspig@users.noreply.github.com>
2023-06-15 08:01:14 -0600

    they just twitch but the attraction semi works

--- agent.py
+++ agent.py
@@ -2,58 +2,125 @@ import numpy as np
 import random
 from typing import List
 import math
+from math import cos, sin, pi, atan2
 
 # Simulation parameters
-v0           = 5      # velocity
-eta          = 0.6      # random fluctuation in angle (in radians)
-L            = 10        # size of box
-R            = 0.5      # interaction radius
-dt           = 0.1      # time step
-Nt           = 200      # number of time steps
-N            = 20      # number of birds
+S = 1      # velocity
+eta = 0.6      # random fluctuation in angle (in radians)
+L = 100        # size of box
+R = 0.5      # interaction radius
+dt = 0.1      # time step
+Nt = 200      # number of time steps
 plotRealTime = True
-k            = 0.5        # Attraction factor
-a            = 1        # agent is visible to other agent in time a
+k = 0.5        # Attraction factor
+a = 1        # agent is visible to other agent in time a
+
 
 class Agent:
-    theta  = random.randint(-1 * int(np.pi), int(np.pi))
-    def __init__(self,x,y) -> None:
+
+    def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
-        self.c = np.array([self.x,self.y])
-        
-        self.vx = v0 * np.cos(self.theta)
-        self.vy = v0 * np.sin(self.theta)
+        self.c = np.array([self.x, self.y])
 
-    def move(self):    
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-       
-    def update_velocity(self):
-        self.vx = v0 * np.cos(self.theta)
-        self.vy = v0 * np.sin(self.theta)
-        
+        self.theta = round(random.uniform(-math.pi, math.pi), 2)
+        # self.vx = S * np.cos(self.theta)
+        # self.vy = S * np.sin(self.theta)
+        self.V = np.array([S * cos(self.theta), S * sin(self.theta)])
+        self.W = self.theta
+
+    def move(self):
+        # as a reminder this is off the previous self.theta
+        # theta_t+1 = theta_t + W * dt
+        # x_t+1 = x_t + S*cos(theta_t) * dt
+        # the reason this is done with the old theta is that
+        # self.x += self.x + S*cos(self.theta) * dt
+        # self.y += self.y + S*sin(self.theta) * dt
+        # self.V = self.theta + self.W * dt  # this is the new theta
+        self.x = self.x + self.V[0] * dt
+        self.y = self.y + self.V[1] * dt
+        self.V[0] = S * np.cos(self.theta)
+        self.V[1] = S * np.sin(self.theta)
+        self.theta = self.theta + self.W * dt
+
     def update_angular_velocity(self, foreigners):
-        info_dict = {'info1': np.array([0,0]), 'info2': np.array([0,0]), 'scalar_constant': 1}
+        repulsion_info = np.array([0.0, 0.0])
+        sum_of_V = np.array([0.0, 0.0])
+        attraction_info = np.array([0.0, 0.0])
+        # info2 = np.array([0.0, 0.0])
+        # info1 = np.array([0.0, 0.0])
+        # attraction_scalar_constant = 1.0
         for agent in foreigners:
             distance = math.dist(self.c, agent.c)
-            
-            try:
-                p = min(1, 1/distance)
-            except ZeroDivisionError as e:
-                p = 1
-                
-            if np.random.default_rng().binomial(1, p, 1) > 0:
-                info_dict['info1'] += (agent.c - self.c)
-                info_dict['scalar_constant'] += np.linalg.norm(agent.c - self.c)
-                
-        u3 = info_dict['info1']/info_dict['scalar_constant']
-        w = k * ((np.arctan2(u3[1], u3[0])) - self.theta)
-        
-        self.theta = w
-        
+
+            if 1 > distance:
+                # do the repulsion calculation
+                repulsion_info = (self.c - agent.c) / \
+                    (np.linalg.norm(self.c - agent.c))**2
+            elif 8 > distance:
+                # do the orientation calculation
+                sum_of_V += agent.V
+            elif 80 > distance:
+                # do the attraction calculation
+                attraction_info += (agent.c - self.c)
+            else:
+                # do nothing
+                x = 0
+
+            # elif distance > 3:
+            #     # orientation calculation
+            #     sum_of_V += np.array([agent.vx, agent.vy])
+            # else:
+            #     # repulsion calculation
+            #     info_repulsion = (self.c - agent.c) / \
+            #         (np.linalg.norm(self.c - agent.c))**2
+            #     info_dict['info1'] += info_repulsion
+
+            # this will be helpful for the bottom of the repulsion:                 attraction_scalar_constant += np.linalg.norm(
+                # agent.c - self.c)
+
+        u3 = attraction_info/np.linalg.norm(attraction_info) if np.linalg.norm(attraction_info) != 0 else np.array([0.0, 0.0])
+        u2 = (self.V + sum_of_V)/np.linalg.norm(self.V + sum_of_V) if np.linalg.norm(self.V + sum_of_V) != 0 else np.array([0.0, 0.0])
+        u1 = repulsion_info
+        U = u1 + u2 + u3
+        self.W=k * ((atan2(U[1], U[0])) - self.theta)
+
+        # self.theta = self.theta + w * dt
 
 
-   
+# class Agent:
+#     theta  = random.randint(-1 * int(np.pi), int(np.pi))
+#     def __init__(self,x,y) -> None:
+#         self.x = x
+#         self.y = y
+#         self.c = np.array([self.x,self.y])
 
- 
+#         self.vx = S * np.cos(self.theta)
+#         self.vy = S * np.sin(self.theta)
+
+#     def move(self):
+#         self.x += self.vx * dt
+#         self.y += self.vy * dt
+
+#     def update_velocity(self):
+#         self.vx = S * np.cos(self.theta)
+#         self.vy = S * np.sin(self.theta)
+
+#     def update_angular_velocity(self, foreigners):
+#         info_dict = {'info1': np.array([0,0]), 'info2': np.array([0,0]), 'scalar_constant': 1}
+#         for agent in foreigners:
+#             distance = math.dist(self.c, agent.c)
+
+#             try:
+#                 p = min(1, 1/distance)
+#             except ZeroDivisionError as e:
+#                 p = 1
+
+#             if np.random.default_rng().binomial(1, p, 1) > 0:
+#                 info_dict['info1'] += (agent.c - self.c)
+#                 info_dict['scalar_constant'] += np.linalg.norm(agent.c - self.c)
+
+#         u3 = info_dict['info1']/info_dict['scalar_constant']
+#         w = k * ((np.arctan2(u3[1], u3[0])) - self.theta)
+
+#         self.theta = w
--- main.py
+++ main.py
@@ -6,58 +6,61 @@ from agent import Agent
 from typing import List
 
 # Simulation parameters
-L            = 100        # size of box
-R            = 0.5      # interaction radius
-dt           = 0.01      # time step
-Nt           = 200      # number of time steps
-N            = 100      # number of birds
+L = 1000        # size of box
+dt = 0.1      # time step
+Nt = 200      # number of time steps
+N = 20    # number of agents
 plotRealTime = True
 
 # Simulation Main Loop
+
+
 def main():
-    
-    # Create birds
-	agent = Agent(random.randint(1,(L/2)), random.randint(1,(L/2)))
-	agents = [Agent(random.randint(1,(L/2)), random.randint(1,(L/2))) for i in range(N)]
-	foreigners = [other_agent for other_agent in agents if other_agent is not agent]
-   
+
+    # Create agents
+    # initiating an agent in the bottom left quarter of the window
+    # agent = Agent(random.randint(1, (L/2)), random.randint(1, (L/2)))
+    agents = [Agent(random.randint(1, (L/2)), random.randint(1, (L/2))) for i in range(N)]
+    foreigners = [other_agent for other_agent in agents]  # this basically removes the duplication of the starting agent
+    # other_agent for other_agent in agents if other_agent is not agent]  # this basically removes the duplication of the starting agent
+
     # Prep figure
-	fig = plt.figure(figsize=(6,6), dpi=96)
-	ax = plt.gca()
- 
-	for i in range(Nt):
+    fig = plt.figure(figsize=(6, 6), dpi=96)
+    ax = plt.gca()
 
-		# move
-		for agent in agents:
-			agent.move()
+    for i in range(Nt):
 
-		# List of x and y values of birds to put in quiver
-		x = [agent.x for agent in agents]
-		y = [agent.y for agent in agents]
-		vx = [agent.vx for agent in agents]
-		vy = [agent.vy for agent in agents]
+        # List of x and y values of agents to put in quiver
+        x = [agent.x % L for agent in agents]
+        y = [agent.y % L for agent in agents]
+        vx = [agent.V[0] for agent in agents]
+        vy = [agent.V[1] for agent in agents]
 
-		# plot in real time
-		if plotRealTime or (i == Nt-1):
-			plt.cla()
-			plt.quiver(x,y,vx,vy,color='r')
-			ax.set(xlim=(0, L), ylim=(0, L))
-			ax.set_aspect('equal')	
-			plt.pause(0.001)
-	
-		# update velocities
-		for agent in agents:
-			agent.update_velocity()
-			
-		# Update angular velocity 
-		for agent in agents:
-			agent.update_angular_velocity(foreigners)
-   
-			
-	# Save figure
-	plt.savefig('activematter.png',dpi=240)
-	plt.show()
- 
+        testx = x[0]
+        testy = y[0]
+        testvx = vx[0]
+        testvy = vy[0]
+        # plot all agents
+        if plotRealTime or (i == Nt-1):
+            plt.cla()
+            plt.quiver(x, y, vx, vy, color='r')
+            plt.quiver(testx, testy, testvx, testvy, color='b')
+            ax.set(xlim=(0, L), ylim=(0, L))
+            ax.set_aspect('equal')
+            plt.pause(0.001)
+
+        # Update angular velocity
+        for agent in agents:
+            agent.update_angular_velocity([foreigner for foreigner in agents if foreigner is not agent])
+
+            # move
+        for agent in agents:
+            agent.move()
+
+    # Save figure
+    plt.savefig('activematter.png', dpi=240)
+    plt.show()
+
+
 if __name__ == '__main__':
     main()
-
