b48ced62a114bcd0460741ab1147fb8bbc50a197
Suzana <suzanasp1026@gmail.com>
2023-06-16 12:52:45 -0600

    small changes

--- agent.py
+++ agent.py
@@ -5,9 +5,9 @@ import math
 
 # Simulation parameters
 v0           = 5      # velocity
-eta          = 0.6      # random fluctuation in angle (in radians)
 L            = 10        # size of box
-R            = 0.5      # interaction radius
+Rr            = 1     # interaction radius
+Ro            = 8
 dt           = 0.1      # time step
 Nt           = 200      # number of time steps
 N            = 20      # number of birds
@@ -24,7 +24,8 @@ class Agent:
         
         self.vx = v0 * np.cos(self.theta)
         self.vy = v0 * np.sin(self.theta)
-
+        self.v = np.array([self.vx, self.vy])
+                          
     def move(self):    
         self.x += self.vx * dt
         self.y += self.vy * dt
@@ -32,21 +33,51 @@ class Agent:
     def update_velocity(self):
         self.vx = v0 * np.cos(self.theta)
         self.vy = v0 * np.sin(self.theta)
+        self.v = np.array([self.vx, self.vy])
         
     def update_angular_velocity(self, foreigners):
-        info_dict = {'info1': np.array([0,0]), 'info2': np.array([0,0]), 'scalar_constant': 1}
-        for agent in foreigners:
-            distance = math.dist(self.c, agent.c)
+        info_dict = {
+            'info1': np.array([0, 0], dtype=np.float64),
+            'info2': np.array([0, 0], dtype=np.float64),
+            'scalar_constant': 1,
+            'info3': np.array([0, 0], dtype=np.float64),
+            'info4': np.array([0, 0], dtype=np.float64)
+        }
+
+        for other_agent in foreigners:
+            distance = math.dist(self.c, other_agent.c)
             
             try:
                 p = min(1, 1/distance)
             except ZeroDivisionError as e:
                 p = 1
                 
+            # For formula 7
             if np.random.default_rng().binomial(1, p, 1) > 0:
-                info_dict['info1'] += (agent.c - self.c)
-                info_dict['scalar_constant'] += np.linalg.norm(agent.c - self.c)
+                info_dict['info1'] += (other_agent.c - self.c)
+                info_dict['scalar_constant'] += np.linalg.norm(other_agent.c - self.c)
+
+                """
+                Umask = np.array([False, False])  # Example Umask array
+                # For formula 5
+                if np.linalg.norm(self.c - other_agent.c) <= Rr:       
+                    # Check if the slice is empty before computing the mean
+                    if np.any(~Umask):
+                        info_dict['info3'] += (other_agent.c - self.c)/(np.linalg.norm(other_agent.c - self.c)**2)
+                    else:
+                        amean = 0  # Set a default value or handle the case when the slice is empty
+
+
+                   
                 
+                # For formula 6
+                if np.linalg.norm(self.c - other_agent.c) <= Ro:   
+                    info_dict['info4'] +=  other_agent.v
+                
+
+        u1 = -1 * info_dict['info3']
+        u2 = (self.v + info_dict['info4'])/np.linalg.norm(self.v + info_dict['info4'])
+        """
         u3 = info_dict['info1']/info_dict['scalar_constant']
         w = k * ((np.arctan2(u3[1], u3[0])) - self.theta)
         
